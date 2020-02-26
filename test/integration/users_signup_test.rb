b09f70e5acbc4aa26f234a8f24e588705888fc06
conejitomaya <eduardobonillac@gmail.com>
2020-02-26 12:29:06 -0600

    sessions added

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,8 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
@@ -24,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
